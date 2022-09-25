cbb19417b553716514ab1418cad5e1c3ce8bf0d7
brianmwevi <mwevibrian@gmail.com>
2022-09-25 21:09:15 +0300

    Initial commit

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+const weeks = {
+  weeks1: new Date(2022,10,2),
+  weeks2: new Date(2022,10,9),
+  weeks3: new Date(2022,10,16),
+  weeks4: new Date(2022,10,23),
+  weeks5: new Date(2022,10,30),
+  weeks6: new Date(2022,10,9),
+  weeks7: new Date(2022,10,9),
+  
+}
+
